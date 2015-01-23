87f90d2f31d05cb23dd81559f22eb083c505a92e
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-01-23 12:50:28 -0500

    //This is my minimum homework.

---
+++ minimum.js
@@ -0,0 +1,24 @@
+
+
+//Katie Harold
+//CMP237
+//Minimum Ch. 3
+
+
+var min = function(x,y) {
+	if(x<y) {
+		return x;
+	}
+	else {
+		return y;
+	}
+}
+
+console.log(min(0,10));
+// 0
+console.log(min(0,-10));
+// -10
+
+
+
+
